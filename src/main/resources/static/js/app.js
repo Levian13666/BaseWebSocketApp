4564736c860f90ecb3b0a5b4bf0de896361f0ee2
oleg.ilechko <oleg.ilechko@vitech.com.ua>
2016-05-20 20:19:59 +0300

    Date picker scroll

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,11 +2,16 @@ angular.module('main', []).controller('mainController', ['$scope', '$http', Main
 
 function MainController($scope, $http) {
 
-    var width = 400;
+    var width = 420;
     var height = 100;
     var margin = 40;
     var yearMarginStart = margin - 7;
     var yearMarginEnd = margin - 35;
+
+    var monthCircleRadius = 5;
+
+    var datePickerMargin = 40;
+    var visibleMonthCount = 7;
 
     var monthFormat = d3.time.format("%b");
     var yearFormat = d3.time.format("%Y");
@@ -15,23 +20,26 @@ function MainController($scope, $http) {
         .attr('width', width)
         .attr('height', height);
 
-    var g = svg.append('g');
+    var dateRange = d3.time.month.range(new Date(2001, 1, 1), new Date());
 
-    var dateRange = d3.time.month.range(new Date(2015, 8, 1), new Date());
 
-    g.append('line')
+    svg.append('line')
         .attr('x1', margin)
         .attr('y1', margin)
         .attr('x2', function() {
-            return margin * (dateRange.length - 1) + margin})
+            return margin * (visibleMonthCount - 1) + margin - monthCircleRadius})
         .attr('y2', margin)
-        .attr('class', 'month-line');
+        .attr('class', 'month-line')
+        .attr("transform", "translate(" + datePickerMargin + ", 0)");
 
-    g.selectAll("date-month")
+    var datePicker = svg.append('g')
+        .attr("transform", "translate(" + datePickerMargin + ", 0)");
+
+    datePicker.selectAll(".date-month")
         .data(dateRange)
         .enter()
         .append("circle")
-        .attr("r", 5)
+        .attr("r", monthCircleRadius)
         .attr('cx', function(d, i) {return margin * i + margin;})
         .attr('cy', margin)
         .attr('id', function(d, i) {return 'date-month-' + i})
@@ -42,9 +50,9 @@ function MainController($scope, $http) {
             return monthFormat(d) == 'Jan';
         });
 
-    g.selectAll(".date-month").each(function(d, i){
+    datePicker.selectAll(".date-month").each(function(d, i){
         if (monthFormat(d) == 'Jan') {
-            g.insert('line', ":first-child")
+            datePicker.insert('line', ":first-child")
                 .attr('x1', function () {
                     return margin * i + margin;
                 })
@@ -55,7 +63,7 @@ function MainController($scope, $http) {
                 .attr('y2', yearMarginEnd)
                 .attr('class', 'year-line');
 
-            g.insert('text', ":first-child")
+            datePicker.insert('text', ":first-child")
                 .attr('x', function () {
                     return margin * i + margin + 5;
                 })
@@ -65,7 +73,7 @@ function MainController($scope, $http) {
         }
     });
 
-    g.selectAll("label")
+    datePicker.selectAll("label")
         .data(dateRange)
         .enter()
         .append("text")
@@ -116,6 +124,79 @@ function MainController($scope, $http) {
         .enter().append("stop")
         .attr("offset", function(d) { return d.offset; })
         .attr("stop-color", function(d) { return d.color; });
+
+
+    var lastMonthIndex = svg.selectAll(".date-month")[0].length - 1;
+    var scrollPosition = visibleMonthCount - 1;
+
+    svg.append("rect")
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("width", margin + datePickerMargin - monthCircleRadius * 2)
+        .attr("height", height)
+        .attr("class", "scroll-button-panel");
+
+    svg.append("rect")
+        .attr("x", margin * visibleMonthCount + datePickerMargin + monthCircleRadius * 2)
+        .attr("y", 0)
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "scroll-button-panel");
+
+    var leftScroll = svg.append("g");
+
+    leftScroll.append("path")
+        .attr("transform", "translate(45," + (height / 2) + ") rotate(90)")
+        .attr('class', 'scroll-button')
+        .attr("d", d3.svg.symbol().type("triangle-down").size(100))
+        .on('click', function(d, i) {
+            if (scrollPosition >= visibleMonthCount) {
+                scrollPosition--;
+            }
+            scrollTo(scrollPosition);
+        });
+
+    var rightScroll = svg.append("g");
+
+    rightScroll.append("path")
+        .attr("transform", "translate(" + (width - 40) + "," + (height / 2) + ") rotate(-90)")
+        .attr('class', 'scroll-button')
+        .attr("d", d3.svg.symbol().type("triangle-down").size(100))
+        .on('click', function(d, i) {
+            if (scrollPosition < lastMonthIndex) {
+                scrollPosition++;
+            }
+            scrollTo(scrollPosition);
+        });
+
+    var rightEndScroll = svg.append("g");
+
+    rightEndScroll.append("path")
+        .attr("transform", "translate(" + (width - 10) + "," + (height / 2) + ") rotate(-90)")
+        .attr('class', 'scroll-button')
+        .attr("d", d3.svg.symbol().type("triangle-down").size(100))
+        .on('click', function(d, i) {
+            scrollPosition = lastMonthIndex;
+            scrollTo(scrollPosition);
+        });
+
+    var leftEndScroll = svg.append("g");
+
+    leftEndScroll.append("path")
+        .attr("transform", "translate(10," + (height / 2) + ") rotate(90)")
+        .attr('class', 'scroll-button')
+        .attr("d", d3.svg.symbol().type("triangle-down").size(100))
+        .on('click', function(d, i) {
+            scrollPosition = visibleMonthCount - 1;
+            scrollTo(scrollPosition);
+        });
+
+    function scrollTo(monthIndex) {
+        monthIndex = monthIndex < visibleMonthCount ? visibleMonthCount - 1 : monthIndex;
+        var month = svg.selectAll(".date-month")[0][monthIndex];
+        var x = -d3.select(month).attr("cx") + visibleMonthCount * margin;
+        datePicker.transition().attr("transform", "translate(" + (datePickerMargin + x) + ", 0)");
+    }
 
     /*Shadow Filter*/
     var defs = svg.append("defs");
